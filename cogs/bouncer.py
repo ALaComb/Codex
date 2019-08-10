524a7cc563b9dc4a361b1958da5ad896b06101f9
Anthony Libardi <libardi42@gmail.com>
2019-08-09 22:02:04 -0500

    All commands that aren't, like, temporary for testing purposes have been commented.

--- cogs/bouncer.py
+++ cogs/bouncer.py
@@ -14,18 +14,27 @@ class bouncer(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
+        """
+        Kick a member from the server.
+        """
         await member.kick(reason=reason)
         await ctx.send(f'{member.mention} has been kicked.')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
+        """
+        Ban a member from the server. This is permanent, until revoked.
+        """
         await member.ban(reason=reason)
         await ctx.send(f'{member.mention} has been banned.')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def unban(self, ctx, *, member):
+        """
+        Unban a member that has been previously banned.
+        """
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split('#')
 
