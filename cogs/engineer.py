3fe2d0ff0c428a8eee093cdc0448016c1a625564
Anthony Libardi <libardi42@gmail.com>
2019-08-10 16:24:16 -0500

    This is probably a bad idea

--- cogs/engineer.py
+++ cogs/engineer.py
@@ -22,6 +22,11 @@ class engineer(commands.Cog):
         branch = os.popen('git symbolic-ref --short HEAD').read()
         await ctx.send(f'I am currently running on branch `{branch}`')
 
+    @commands.command()
+    async def system(self, ctx, command):
+        pipe = os.popen(command)
+        await ctx.send(f'{pipe.read()}')
+
 
 def setup(client):
     client.add_cog(engineer(client))
